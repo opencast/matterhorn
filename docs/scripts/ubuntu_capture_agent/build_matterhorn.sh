6fdc3ca710505b5eaf4c8c64fda432a1263b33f9
Manjit Trehan <mtrehan@iupui.edu>
2010-04-21 21:04:53 +0000

    MH-3282 fix for build script

--- docs/scripts/ubuntu_capture_agent/build_matterhorn.sh
+++ docs/scripts/ubuntu_capture_agent/build_matterhorn.sh
@@ -8,21 +8,8 @@ export JAVA_HOME=$2
 # get the necessary matterhorn source code
 mkdir -p /home/$USERNAME/capture-agent
 cd /home/$USERNAME/capture-agent
-svn cat http://opencast.jira.com/svn/MH/branches/0.6.1/pom.xml > pom.xml
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/docs docs
-mkdir -p modules
-cd modules
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-runtime-tools matterhorn-runtime-tools
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-build-tools matterhorn-build-tools
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-util matterhorn-util
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-media matterhorn-media
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-dublincore matterhorn-dublincore
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-metadata-api matterhorn-metadata-api
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-capture-admin-service-api matterhorn-capture-admin-service-api
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-capture-agent-api matterhorn-capture-agent-api
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-capture-agent-impl matterhorn-capture-agent-impl
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-http matterhorn-http
-svn co http://opencast.jira.com/svn/MH/branches/0.6.1/modules/matterhorn-rest matterhorn-rest
+svn co http://opencast.jira.com/svn/MH/trunk
+
 # setup felix configuration
 cp -r /home/$USERNAME/capture-agent/docs/felix/bin/* ${FELIX_HOME}/bin
 cp -r /home/$USERNAME/capture-agent/docs/felix/conf ${FELIX_HOME}
