9a37b016ee09bc07b8ccad22952d00d3946770cf
Nils Birnbaum <nils.birnbaum@uni-osnabrueck.de>
2010-04-23 12:07:36 +0000

    fix MH-3347

--- docs/scripts/ubuntu_capture_agent/build_matterhorn.sh
+++ docs/scripts/ubuntu_capture_agent/build_matterhorn.sh
@@ -8,12 +8,12 @@ export JAVA_HOME=$2
 # get the necessary matterhorn source code
 mkdir -p /home/$USERNAME/capture-agent
 cd /home/$USERNAME/capture-agent
-svn co http://opencast.jira.com/svn/MH/tags/0.6.1
+svn co http://opencast.jira.com/svn/MH/tags/0.6.1 .
 
 # setup felix configuration
 cp -r /home/$USERNAME/capture-agent/docs/felix/bin/* ${FELIX_HOME}/bin
 cp -r /home/$USERNAME/capture-agent/docs/felix/conf ${FELIX_HOME}
 
-cd ..
+
 mvn clean install -Pcapture -DdeployTo=${FELIX_HOME}/load
 exit $?
