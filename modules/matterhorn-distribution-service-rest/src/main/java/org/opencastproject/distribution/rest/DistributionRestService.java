250934cb38868659b9ab0dd24431cdc67835dd85
Manjit Trehan <mtrehan@iupui.edu>
2010-06-29 19:44:23 +0000

    MH-4373 Merged with 1.0.x branch

--- modules/matterhorn-distribution-service-rest/src/main/java/org/opencastproject/distribution/rest/DistributionRestService.java
+++ modules/matterhorn-distribution-service-rest/src/main/java/org/opencastproject/distribution/rest/DistributionRestService.java
@@ -133,7 +133,7 @@ public class DistributionRestService {
 
   private String generateMediaPackage() {
     try {
-      String template = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("manifest.xml"));
+      String template = IOUtils.toString(getClass().getClassLoader().getResourceAsStream("manifest.xml"), "UTF-8");
       return template.replaceAll("@SAMPLES_URL@", serverUrl + "/workflow/samples");
     } catch (IOException e) {
       logger.warn("Unable to load the sample mediapackage");
