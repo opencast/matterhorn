738cd0b62f301587743fe2ceaab43dde1b06a5fb
Manjit Trehan <mtrehan@iupui.edu>
2010-07-28 20:02:06 +0000

    MH-5039 Merged rev 8414 with 1.0.x branch

--- modules/matterhorn-engage-ui/src/main/resources/ui/js/watch.js
+++ modules/matterhorn-engage-ui/src/main/resources/ui/js/watch.js
@@ -25,7 +25,8 @@ Opencast.Watch = (function ()
           mediaResolutionOne      = "",
           mediaResolutionTwo      = "",
           coverUrlOne             = "",
-          coverUrlTwo             = "";
+          coverUrlTwo             = "",
+          slideLength             = 0;
 
 
         var mediaPackageId = Opencast.engage.getMediaPackageId();
@@ -132,8 +133,14 @@ Opencast.Watch = (function ()
 
           mediaResolutionOne = mediaResolutionOne === null ? '' : mediaResolutionOne;
           mediaResolutionTwo = mediaResolutionTwo === null ? '' : mediaResolutionTwo;
-
-          Opencast.Player.setMediaURL(coverUrlOne, coverUrlTwo, mediaUrlOne, mediaUrlTwo, mimetypeOne, mimetypeTwo, PLAYERSTYLE);
+          
+          // init the segements
+          Opencast.segments.initialize();
+          
+          
+          slideLength = Opencast.segments.getSlideLength();
+          
+          Opencast.Player.setMediaURL(coverUrlOne, coverUrlTwo, mediaUrlOne, mediaUrlTwo, mimetypeOne, mimetypeTwo, PLAYERSTYLE, slideLength);
 
           if (mediaUrlOne !== '' && mediaUrlTwo !== '')
           {
@@ -187,7 +194,7 @@ Opencast.Watch = (function ()
             Opencast.Player.stopFastForward();
           });
           
-          Opencast.segments.initialize();
+         
           
           Opencast.search.initialize();
 
