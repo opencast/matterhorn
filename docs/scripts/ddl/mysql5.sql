f7de5b1a83955706083e05f3cb5605c9de135c1f
Tobias Wunden <tobias@entwinemedia.com>
2011-01-14 09:45:15 +0000

    MH-6259 -- Moving messed-up branch into the right place

---
+++ docs/scripts/ddl/mysql5.sql
@@ -0,0 +1,158 @@
+CREATE TABLE `ANNOTATION` (
+  `ID` bigint(20) NOT NULL,
+  `OUTPOINT` int(11) default NULL,
+  `INPOINT` int(11) default NULL,
+  `MEDIA_PACKAGE_ID` varchar(36) collate utf8_unicode_ci default NULL,
+  `SESSION_ID` varchar(255) collate utf8_unicode_ci default NULL,
+  `CREATED` datetime default NULL,
+  `USER_ID` varchar(255) collate utf8_unicode_ci default NULL,
+  `LENGTH` int(11) default NULL,
+  `ANNOTATION_VAL` text collate utf8_unicode_ci,
+  `ANNOTATION_TYPE` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `CAPTURE_AGENT_STATE` (
+  `NAME` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `STATE` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `CAPABILITIES` tinyblob,
+  `LAST_HEARD_FROM` bigint(20) NOT NULL,
+  `URL` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `DICTIONARY` (
+  `TEXT` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `LANGUAGE` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `WEIGHT` double default NULL,
+  `COUNT` bigint(20) default NULL,
+  `STOPWORD` tinyint(1) default '0',
+  PRIMARY KEY  (`TEXT`,`LANGUAGE`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SCHED_EVENT` (
+  `EVENT_ID` bigint(20) NOT NULL,
+  `STARTDATE` datetime default NULL,
+  `RESOURCES` varchar(255) collate utf8_unicode_ci default NULL,
+  `SERIES` varchar(255) collate utf8_unicode_ci default NULL,
+  `SUBJECT` varchar(255) collate utf8_unicode_ci default NULL,
+  `ENDDATE` datetime default NULL,
+  `RECURRENCEPATTERN` varchar(255) collate utf8_unicode_ci default NULL,
+  `CREATOR` varchar(255) collate utf8_unicode_ci default NULL,
+  `TITLE` varchar(255) collate utf8_unicode_ci default NULL,
+  `DURATION` bigint(20) default NULL,
+  `RECURRENCE` varchar(255) collate utf8_unicode_ci default NULL,
+  `DESCRIPTION` text collate utf8_unicode_ci,
+  `CONTRIBUTOR` varchar(255) collate utf8_unicode_ci default NULL,
+  `DEVICE` varchar(255) collate utf8_unicode_ci default NULL,
+  `LANGUAGE` varchar(255) collate utf8_unicode_ci default NULL,
+  `LICENSE` varchar(255) collate utf8_unicode_ci default NULL,
+  `SERIESID` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`EVENT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SCHED_METADATA` (
+  `MD_KEY` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `MD_VAL` varchar(255) collate utf8_unicode_ci default NULL,
+  `EVENT_ID` bigint(20) NOT NULL,
+  PRIMARY KEY  (`MD_KEY`,`EVENT_ID`),
+  KEY `FK_SCHED_METADATA_EVENT_ID` (`EVENT_ID`),
+  CONSTRAINT `FK_SCHED_METADATA_EVENT_ID` FOREIGN KEY (`EVENT_ID`) REFERENCES `SCHED_EVENT` (`EVENT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SEQUENCE` (
+  `SEQ_NAME` varchar(50) collate utf8_unicode_ci NOT NULL,
+  `SEQ_COUNT` decimal(38,0) default NULL,
+  PRIMARY KEY  (`SEQ_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SERIES` (
+  `SERIES_ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  `DESCRIPTION` text collate utf8_unicode_ci,
+  PRIMARY KEY  (`SERIES_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SERIES_METADATA` (
+  `METADATA_KEY` varchar(128) collate utf8_unicode_ci NOT NULL,
+  `METADATA_VAL` text collate utf8_unicode_ci,
+  `SERIES_ID` varchar(128) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`METADATA_KEY`,`SERIES_ID`),
+  KEY `FK_SERIES_METADATA_SERIES_ID` (`SERIES_ID`),
+  CONSTRAINT `FK_SERIES_METADATA_SERIES_ID` FOREIGN KEY (`SERIES_ID`) REFERENCES `SERIES` (`SERIES_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `UPLOAD` (
+  `ID` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `total` bigint(20) NOT NULL,
+  `received` bigint(20) NOT NULL,
+  `filename` varchar(255) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `USER_ACTION` (
+  `ID` bigint(20) NOT NULL,
+  `OUTPOINT` int(11) default NULL,
+  `INPOINT` int(11) default NULL,
+  `MEDIA_PACKAGE_ID` varchar(128) collate utf8_unicode_ci default NULL,
+  `SESSION_ID` varchar(255) collate utf8_unicode_ci default NULL,
+  `CREATED` datetime default NULL,
+  `USER_ID` varchar(255) collate utf8_unicode_ci default NULL,
+  `LENGTH` int(11) default NULL,
+  `TYPE` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `HOST_REGISTRATION` (
+  `HOST` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `MAINTENANCE` tinyint(1) NOT NULL default '0',
+  `MAX_JOBS` int(11) NOT NULL,
+  `ONLINE` tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (`HOST`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `SERVICE_REGISTRATION` (
+  `SERVICE_TYPE` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `PATH` varchar(255) collate utf8_unicode_ci NOT NULL,
+  `JOB_PRODUCER` tinyint(1) NOT NULL default '0',
+  `ONLINE` tinyint(1) NOT NULL default '0',
+  `HOST` varchar(255) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`SERVICE_TYPE`,`HOST`),
+  KEY `FK_SERVICE_REGISTRATION_HOST` (`HOST`),
+  CONSTRAINT `FK_SERVICE_REGISTRATION_HOST` FOREIGN KEY (`HOST`) REFERENCES `host_registration` (`HOST`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `JOB` (
+  `ID` bigint(20) NOT NULL,
+  `STATUS` int(11) default NULL,
+  `PAYLOAD` text collate utf8_unicode_ci,
+  `DATESTARTED` datetime default NULL,
+  `RUNTIME` bigint(20) default NULL,
+  `VERSION` int(11) default NULL,
+  `DATECOMPLETED` datetime default NULL,
+  `operation` varchar(255) collate utf8_unicode_ci default NULL,
+  `DATECREATED` datetime default NULL,
+  `QUEUETIME` bigint(20) default NULL,
+  `PROCESSOR_SVC_TYPE` varchar(255) collate utf8_unicode_ci default NULL,
+  `PROCESSOR_HOST` varchar(255) collate utf8_unicode_ci default NULL,
+  `CREATOR_SVC_TYPE` varchar(255) collate utf8_unicode_ci default NULL,
+  `CREATOR_HOST` varchar(255) collate utf8_unicode_ci default NULL,
+  PRIMARY KEY  (`ID`),
+  KEY `FK_JOB_CREATOR_SVC_TYPE` (`CREATOR_SVC_TYPE`,`CREATOR_HOST`),
+  KEY `FK_JOB_PROCESSOR_SVC_TYPE` (`PROCESSOR_SVC_TYPE`,`PROCESSOR_HOST`),
+  CONSTRAINT `FK_JOB_PROCESSOR_SVC_TYPE` FOREIGN KEY (`PROCESSOR_SVC_TYPE`, `PROCESSOR_HOST`) REFERENCES `service_registration` (`SERVICE_TYPE`, `HOST`),
+  CONSTRAINT `FK_JOB_CREATOR_SVC_TYPE` FOREIGN KEY (`CREATOR_SVC_TYPE`, `CREATOR_HOST`) REFERENCES `service_registration` (`SERVICE_TYPE`, `HOST`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `JOB_ARG` (
+  `ID` bigint(20) NOT NULL,
+  `argument` text collate utf8_unicode_ci,
+  `listindex` int(11) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `SEQUENCE` VALUES('SEQ_GEN', 50);
+
+CREATE INDEX `DICTIONARY_TEXT` ON `DICTIONARY` (`TEXT`);
+CREATE INDEX `DICTIONARY_LANGUAGE` ON `DICTIONARY` (`LANGUAGE`);
+CREATE INDEX `ANNOTATION_MP_IDX` on `ANNOTATION` (`MEDIA_PACKAGE_ID`);
+CREATE INDEX `USER_ACTION_USER_IDX` on `USER_ACTION` (`USER_ID`);
+CREATE INDEX `USER_ACTION_MP_IDX` on `USER_ACTION` (`MEDIA_PACKAGE_ID`);
---
+++ docs/scripts/ddl/postgres84.sql
@@ -0,0 +1,189 @@
+CREATE TABLE annotation (
+    id bigint NOT NULL,
+    outpoint integer,
+    inpoint integer,
+    media_package_id character varying(36),
+    session_id character varying(255),
+    created timestamp without time zone,
+    user_id character varying(255),
+    length integer,
+    annotation_val text,
+    annotation_type character varying(255)
+);
+
+CREATE TABLE capture_agent_state (
+    name character varying(255) NOT NULL,
+    state character varying(255) NOT NULL,
+    capabilities bytea,
+    last_heard_from bigint NOT NULL,
+    url character varying(255)
+);
+
+CREATE TABLE dictionary (
+    text character varying(255) NOT NULL,
+    language character varying(255) NOT NULL,
+    weight double precision,
+    count bigint,
+    stopword boolean
+);
+
+CREATE TABLE sched_event (
+    event_id bigint NOT NULL,
+    startdate timestamp without time zone,
+    resources character varying(255),
+    series character varying(255),
+    subject character varying(255),
+    enddate timestamp without time zone,
+    recurrencepattern character varying(255),
+    creator character varying(255),
+    title character varying(255),
+    duration bigint,
+    recurrence character varying(255),
+    description text,
+    contributor character varying(255),
+    device character varying(255),
+    language character varying(255),
+    license character varying(255),
+    seriesid character varying(255)
+);
+
+CREATE TABLE sched_metadata (
+    md_key character varying(255) NOT NULL,
+    md_val character varying(255),
+    event_id bigint NOT NULL
+);
+
+CREATE TABLE sequence (
+    seq_name character varying(50) NOT NULL,
+    seq_count numeric(38,0)
+);
+
+CREATE TABLE series (
+    series_id character varying(128) NOT NULL,
+    description text
+);
+
+CREATE TABLE series_metadata (
+    metadata_key character varying(128) NOT NULL,
+    metadata_val text,
+    series_id character varying(128) NOT NULL
+);
+
+CREATE TABLE upload (
+    id character varying(255) NOT NULL,
+    total bigint NOT NULL,
+    received bigint NOT NULL,
+    filename character varying(255) NOT NULL
+);
+
+CREATE TABLE user_action (
+    id bigint NOT NULL,
+    outpoint integer,
+    inpoint integer,
+    media_package_id character varying(128),
+    session_id character varying(255),
+    created timestamp without time zone,
+    user_id character varying(255),
+    length integer,
+    type character varying(255)
+);
+
+CREATE TABLE host_registration (
+    host character varying(255) NOT NULL,
+    maintenance boolean NOT NULL,
+    max_jobs integer NOT NULL,
+    online boolean NOT NULL
+);
+
+CREATE TABLE job (
+    id bigint NOT NULL,
+    status integer,
+    payload text,
+    datestarted timestamp without time zone,
+    runtime bigint,
+    version integer,
+    datecompleted timestamp without time zone,
+    operation character varying(255),
+    datecreated timestamp without time zone,
+    queuetime bigint,
+    creator_svc_type character varying(255),
+    creator_host character varying(255),
+    processor_svc_type character varying(255),
+    processor_host character varying(255)
+);
+
+CREATE TABLE job_arg (
+    id bigint NOT NULL,
+    argument text,
+    listindex integer
+);
+
+CREATE TABLE service_registration (
+    service_type character varying(255) NOT NULL,
+    path character varying(255) NOT NULL,
+    job_producer boolean NOT NULL,
+    online boolean NOT NULL,
+    host character varying(255) NOT NULL
+);
+
+ALTER TABLE ONLY annotation
+    ADD CONSTRAINT annotation_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY capture_agent_state
+    ADD CONSTRAINT capture_agent_state_pkey PRIMARY KEY (name);
+
+ALTER TABLE ONLY dictionary
+    ADD CONSTRAINT dictionary_pkey PRIMARY KEY (text, language);
+
+ALTER TABLE ONLY sched_event
+    ADD CONSTRAINT sched_event_pkey PRIMARY KEY (event_id);
+
+ALTER TABLE ONLY sched_metadata
+    ADD CONSTRAINT sched_metadata_pkey PRIMARY KEY (md_key, event_id);
+
+ALTER TABLE ONLY sequence
+    ADD CONSTRAINT sequence_pkey PRIMARY KEY (seq_name);
+
+ALTER TABLE ONLY series_metadata
+    ADD CONSTRAINT series_metadata_pkey PRIMARY KEY (metadata_key, series_id);
+
+ALTER TABLE ONLY series
+    ADD CONSTRAINT series_pkey PRIMARY KEY (series_id);
+
+ALTER TABLE ONLY upload
+    ADD CONSTRAINT upload_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY user_action
+    ADD CONSTRAINT user_action_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY sched_metadata
+    ADD CONSTRAINT fk_sched_metadata_event_id FOREIGN KEY (event_id) REFERENCES sched_event(event_id);
+
+ALTER TABLE ONLY series_metadata
+    ADD CONSTRAINT fk_series_metadata_series_id FOREIGN KEY (series_id) REFERENCES series(series_id);
+
+ALTER TABLE ONLY host_registration
+    ADD CONSTRAINT host_registration_pkey PRIMARY KEY (host);
+
+ALTER TABLE ONLY job
+    ADD CONSTRAINT job_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY service_registration
+    ADD CONSTRAINT service_registration_pkey PRIMARY KEY (service_type, host);
+
+ALTER TABLE ONLY job
+    ADD CONSTRAINT fk_job_creator_svc_type FOREIGN KEY (creator_svc_type, creator_host) REFERENCES service_registration(service_type, host);
+
+ALTER TABLE ONLY job
+    ADD CONSTRAINT fk_job_processor_svc_type FOREIGN KEY (processor_svc_type, processor_host) REFERENCES service_registration(service_type, host);
+
+ALTER TABLE ONLY service_registration
+    ADD CONSTRAINT fk_service_registration_host FOREIGN KEY (host) REFERENCES host_registration(host);
+
+INSERT INTO SEQUENCE VALUES('SEQ_GEN', 50);
+
+CREATE INDEX dictionary_text on dictionary (text);
+CREATE INDEX dictionary_language on dictionary (language);
+CREATE INDEX annotation_mp_idx on annotation (media_package_id);
+CREATE INDEX user_action_user_idx on user_action (user_id);
+CREATE INDEX user_action_mp_idx on user_action (media_package_id);
