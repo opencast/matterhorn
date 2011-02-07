2434921dc928f7821f9a0a6e43d6af63d380ee58
Adam McKenzie <adam@entwinemedia.com>
2011-02-07 22:56:43 +0000

    MH-6690 Added a check for OSX in both the start_matterhorn.sh and init.d start script, if it is then I checked for $DYLD_FALLBACK_LIBRARY_PATH being empty, and if so then set it by default to /opt/local/lib

--- docs/felix/bin/matterhorn_init_d.sh
+++ docs/felix/bin/matterhorn_init_d.sh
@@ -51,6 +51,18 @@ UTIL_LOGGING_OPTS="-Djava.util.logging.config.file=$FELIX/conf/services/java.uti
 GRAPHICS_OPTS="-Djava.awt.headless=true -Dawt.toolkit=sun.awt.HeadlessToolkit"
 JAVA_OPTS="-Xms256m -Xmx256m -XX:MaxPermSize=128m"
 
+# If this computer is OS X and $DYLD_FALLBACK_LIBRARY_PATH environment variable
+# is not defined, then set it to /opt/local/lib. This is required for the demo
+# capture agent.  
+unameResult=`uname`
+if [ $unameResult = 'Darwin' ];
+then 
+ 	if [ "$DYLD_FALLBACK_LIBRARY_PATH" = "" ];
+	then
+		export DYLD_FALLBACK_LIBRARY_PATH="/opt/local/lib";
+	fi
+fi
+
 FELIX_CACHE="$FELIX/felix-cache"
 
 
