369abbb038f85a29d95569dba3da4fde9fae45aa
Benjamin Wulff <bwulff@uos.de>
2011-02-15 14:47:05 +0000

    MH-6804: reversed odering indicator

--- modules/matterhorn-admin-ui/src/main/resources/ui/js/oc.recordings.js
+++ modules/matterhorn-admin-ui/src/main/resources/ui/js/oc.recordings.js
@@ -498,11 +498,11 @@ ocRecordings = new (function() {
       $(this).removeClass('ui-state-hover');
     })
     .click( function() {
-      var sortDesc = $(this).find('.sort-icon').hasClass('ui-icon-circle-triangle-n');
+      var sortDesc = $(this).find('.sort-icon').hasClass('ui-icon-circle-triangle-s');
       var sortField = ($(this).attr('id')).substr(4);
       $( '#ocRecordingsTable th .sort-icon' )
-      .removeClass('ui-icon-circle-triangle-n')
       .removeClass('ui-icon-circle-triangle-s')
+      .removeClass('ui-icon-circle-triangle-n')
       .addClass('ui-icon-triangle-2-n-s');
       if (sortDesc) {
         ocRecordings.Configuration.sortField = sortField;
@@ -521,9 +521,9 @@ ocRecordings = new (function() {
       var th = $('#sort' + ocRecordings.Configuration.sortField);
       $(th).find('.sort-icon').removeClass('ui-icon-triangle-2-n-s');
       if (ocRecordings.Configuration.sortOrder == 'ASC') {
-        $(th).find('.sort-icon').addClass('ui-icon-circle-triangle-s');
-      } else if (ocRecordings.Configuration.sortOrder == 'DESC') {
         $(th).find('.sort-icon').addClass('ui-icon-circle-triangle-n');
+      } else if (ocRecordings.Configuration.sortOrder == 'DESC') {
+        $(th).find('.sort-icon').addClass('ui-icon-circle-triangle-s');
       }
     }
     // care for items in the table that can be unfolded
