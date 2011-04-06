f4d9c2123436092b4ca469b149cc583b3ecd5824
Adam McKenzie <adam@entwinemedia.com>
2011-04-06 22:50:41 +0000

    MH-7543 - Added a check to start capture to make sure it won't start if it is already past the end time.

--- modules/matterhorn-capture-agent-impl/src/test/java/org/opencastproject/capture/impl/jobs/SchedulingTest.java
+++ modules/matterhorn-capture-agent-impl/src/test/java/org/opencastproject/capture/impl/jobs/SchedulingTest.java
@@ -95,7 +95,7 @@ public class SchedulingTest {
   public void testTest() {
 
     // Setup the job
-    JobDetail job = new JobDetail("starting_capture", Scheduler.DEFAULT_GROUP, StartCaptureJob.class);
+    JobDetail job = new JobDetail("starting_capture", Scheduler.DEFAULT_GROUP, StartCaptureJobTest.class);
 
     long time = TriggerUtils.getNextGivenSecondDate(null, 5).getTime();
     // Create a new trigger Name Group name Start End # of times to repeat Repeat interval
