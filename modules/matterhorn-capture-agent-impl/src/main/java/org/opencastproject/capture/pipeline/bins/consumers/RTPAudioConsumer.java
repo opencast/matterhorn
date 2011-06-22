d69908d749b8692b1099dba291f2e01a5025afb7
Adam McKenzie <adam@entwinemedia.com>
2011-06-22 21:16:42 +0000

    MH-5161 - More checkstyle violations

--- modules/matterhorn-capture-agent-impl/src/main/java/org/opencastproject/capture/pipeline/bins/consumers/RTPAudioConsumer.java
+++ modules/matterhorn-capture-agent-impl/src/main/java/org/opencastproject/capture/pipeline/bins/consumers/RTPAudioConsumer.java
@@ -1,3 +1,18 @@
+/**
+ *  Copyright 2009, 2010 The Regents of the University of California
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ *
+ */
 package org.opencastproject.capture.pipeline.bins.consumers;
 
 import org.opencastproject.capture.pipeline.bins.CaptureDevice;
@@ -178,7 +193,8 @@ public class RTPAudioConsumer extends ConsumerBin {
   
   @Override
   public void linkElements() throws UnableToLinkGStreamerElementsException {
-    Pad sinkPad, srcPad;
+    Pad sinkPad; 
+    Pad srcPad;
     PadLinkReturn padLinkReturn;
     encoder.link(rtpPayloader);
     
@@ -186,28 +202,28 @@ public class RTPAudioConsumer extends ConsumerBin {
     srcPad = rtpPayloader.getStaticPad(GStreamerProperties.SRC);
     sinkPad = gstRTPBin.getRequestPad("send_rtp_sink_0");
     padLinkReturn = srcPad.link(sinkPad);
-    if(!PadLinkReturn.OK.equals(padLinkReturn))
+    if (!PadLinkReturn.OK.equals(padLinkReturn))
       throw new UnableToLinkGStreamerElementsException(captureDevice, rtpPayloader, gstRTPBin);
     
     // Link the RTP Bin to the rtcpUDPSink
     srcPad = gstRTPBin.getRequestPad("send_rtcp_src_0");
     sinkPad = rtcpUDPSink.getStaticPad(GStreamerProperties.SINK);
     padLinkReturn = srcPad.link(sinkPad);
-    if(!PadLinkReturn.OK.equals(padLinkReturn))
+    if (!PadLinkReturn.OK.equals(padLinkReturn))
       throw new UnableToLinkGStreamerElementsException(captureDevice, gstRTPBin, rtcpUDPSink);
     
     // Link RTCP receive to the UDP src
     srcPad = rtcpUDPSink.getStaticPad("src");
-    sinkPad = gstRTPBin.getRequestPad("recv_rtcp_sink_0");;
+    sinkPad = gstRTPBin.getRequestPad("recv_rtcp_sink_0");
     padLinkReturn = srcPad.link(sinkPad);
-    if(!PadLinkReturn.OK.equals(padLinkReturn))
+    if (!PadLinkReturn.OK.equals(padLinkReturn))
       throw new UnableToLinkGStreamerElementsException(captureDevice, gstRTPBin, rtcpUDPSink);
     
     // Link RTP to send through rtpUDPSink
     srcPad = gstRTPBin.getStaticPad("send_rtp_src_0");
     sinkPad = rtpUDPSink.getStaticPad(GStreamerProperties.SINK);
     padLinkReturn = srcPad.link(sinkPad);
-    if(!PadLinkReturn.OK.equals(padLinkReturn))
+    if (!PadLinkReturn.OK.equals(padLinkReturn))
       throw new UnableToLinkGStreamerElementsException(captureDevice, gstRTPBin, rtpUDPSink);
   }
   
