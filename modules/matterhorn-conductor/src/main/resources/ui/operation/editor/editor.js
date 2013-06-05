da5cd6dafb1dec979ff355d81b6d2c27ee6004de
Denis Meyer <denmeyer@uni-osnabruck.de>
2013-06-05 12:05:15 +0000

    MH-8100 MH-9687 added the Artistic License, added some missing copyright headers

--- modules/matterhorn-conductor/src/main/resources/ui/operation/editor/editor.js
+++ modules/matterhorn-conductor/src/main/resources/ui/operation/editor/editor.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2009-2011 The Regents of the University of California Licensed
+ * Copyright 2009-2013 The Regents of the University of California Licensed
  * under the Educational Community License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License. You may obtain a
  * copy of the License at
@@ -11,7 +11,6 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * 
  */
 var editor = editor || {};
 
