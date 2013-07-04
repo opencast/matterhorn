4ad9da25f312c1a4782c28b211af750cb6e76750
Rudiger Rolf <rrolf@uni-osnabrueck.de>
2013-07-04 13:04:24 +0000

    MH-9776 removed unneeded import that caused a build error

--- modules/matterhorn-composer-ffmpeg/src/main/java/org/opencastproject/composer/impl/AbstractCmdlineEncoderEngine.java
+++ modules/matterhorn-composer-ffmpeg/src/main/java/org/opencastproject/composer/impl/AbstractCmdlineEncoderEngine.java
@@ -32,7 +32,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
